5aee64f8146f1e7f2d94adb1e136fc9aeb06dbe5
Aryan <test@test.com>
2021-08-13 13:05:15 +0530

    some changes in UI

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,39 +1,55 @@
-// const input = document.querySelector('input');
-// const btn = document.querySelector('.addTask > button');
-
-// btn.addEventListener('click', addToDo);
-
 axios.get('/list', {
     headers: {
         Authorization: ('Bearer ', localStorage.getItem("token"))
     },
 })
-    .then(function (response) {
-        var list = document.getElementById('list')
-        list = '<tr><th>Tasks</th></tr>'
-        for (i = 0; i < response.data.length; i++) {
-            id = response.data[i]._id
-            list += '<tr>'
-            list += '<td>' + response.data[i].description + '</td>'
-            list += '<td>' + '<button class="fa fa-edit" onclick=updateToDo("' + id + '") action="none" type="submit" value="Update"></button>'
-            list += '<td>' + '<button class="far fa-trash-alt" onclick=deleteToDo("' + id + '") action="none" type="submit" value="Delete"></button>'
-            list += '</tr>'
-        }
-        $('#dialog').hide();
-        $('#target').click(function updateToDo() {
-          $('#dialog').show();
-          $('#dialog').dialog();
-        });
-        document.getElementById('list').innerHTML = list
-        let form = document.getElementById('updateToDo');
-    })
-    .catch(function (error) {
-        if (error.response)
-            console.log(localStorage.token)
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-    });
+.then(function (response) {
+  console.log(response);
+  console.log(response.data)
+
+  var list_pending = document.getElementById('pending')
+  var list_completed = document.getElementById('completed')
+  list_pending = `<h3 id="pending">Pending</h3>`
+  list_completed = `<h3 id="completed">Completed</h3>`
+
+  for (i=0; i< response.data.length; i++)
+  {
+    if(response.data[i].completed == false)
+    {
+    id = response.data[i]._id
+    console.log(id)
+    list_pending += `<li id="${id}">${response.data[i].description}`
+    list_pending += `<form>
+                  <button><i class="fa fa-check"></i></button>
+                  <button onclick=deleteToDo("${id}") action="none" type="submit" value="Delete"><i class="far fa-trash-alt"></i></button>
+                  <button onclick=updateToDo("${id}") action="none" type="submit" value="Update"><i class="fa fa-edit"></i></button>
+            </form></li>`
+    }
+  }
+  for (i=0; i< response.data.length; i++)
+  {
+    if(response.data[i].completed == true)
+    {
+    id = response.data[i]._id
+    console.log(id)
+    list_completed += `<li id="${id}">${response.data[i].description}`
+    list_completed += `<form>
+                  <button><i class="fa fa-check"></i></button>
+                  <button onclick=deleteToDo("${id}") action="none" type="submit" value="Delete"><i class="far fa-trash-alt"></i></button>
+                  <button onclick=updateToDo("${id}") action="none" type="submit" value="Update"><i class="fa fa-edit"></i></button>
+            </form></li>`
+    }
+  }
+  document.getElementById('pending').innerHTML = list_pending
+  document.getElementById('completed').innerHTML = list_completed
+  let form = document.getElementById('updateToDo');
+
+})
+.catch(function (error) {
+  if (error.response) 
+    console.log(localStorage.token)
+    console.log(error.response.data);
+});
 
 function addToDo() {
     const description = document.getElementById('description').value;
